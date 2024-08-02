8cf9b306ad7c434948ffd4392976518dc8ca2b7b
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-02 18:55:52 -0400

    added questions

--- app.js
+++ app.js
@@ -456,3 +456,5 @@ var searchInsert = function(nums, target) {
 console.log(searchInsert([1, 3, 5, 6], 5)); // Output: 2
 console.log(searchInsert([1, 3, 5, 6], 2)); // Output: 1
 console.log(searchInsert([1, 3, 5, 6], 7)); // Output: 4
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
