adb625cf280f6581cd2c443098abdbac59a943df
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-01 22:42:16 -0400

    added questions and answer for js and python

--- app.js
+++ app.js
@@ -427,3 +427,32 @@ console.log(strStr("sadbutsad", "sad"));  // Output: 0
 console.log(strStr("leetcode", "leeto")); // Output: -1
 
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+// Example usage:
+console.log(searchInsert([1, 3, 5, 6], 5)); // Output: 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // Output: 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // Output: 4
