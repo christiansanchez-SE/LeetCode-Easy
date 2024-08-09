7c4d31725a60239999d51273c75fb11da1fe9cdc
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-08 22:12:49 -0400

    added py question and answer

--- pythonScripts.py
+++ pythonScripts.py
@@ -952,3 +952,61 @@ print(sol.mySqrt(4))  # Output: 2
 print(sol.mySqrt(8))  # Output: 2
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# Climbing Stairs
+
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+ 
+
+# Constraints:
+
+# 1 <= n <= 45
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # Initialize base cases
+        first = 1
+        second = 2
+        
+        # Calculate the number of ways for each step from 3 to n
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+        
+        return second
+
+solution = Solution()
+print(solution.climbStairs(2))  # Output: 2
+print(solution.climbStairs(3))  # Output: 3
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
