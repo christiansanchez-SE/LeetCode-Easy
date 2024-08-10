2f1b7f36b66dbd5f3afdc8e3a812d7db7e477c5d
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-10 11:37:28 -0400

    added questions for py and js and answers as well

--- app.js
+++ app.js
@@ -648,3 +648,37 @@ var deleteDuplicates = function(head) {
 };
 
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    // Start from the end of both arrays
+    let p1 = m - 1;  // Pointer for nums1
+    let p2 = n - 1;  // Pointer for nums2
+    let p = m + n - 1;  // Pointer for the end of the merged array
+    
+    // While there are still elements to compare in nums1 and nums2
+    while (p1 >= 0 && p2 >= 0) {
+        // Compare and place the larger element at the end of nums1
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+    
+    // If there are still elements left in nums2, copy them
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+        p--;
+    }
+};
