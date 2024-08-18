454c18012a30b54a41883bc882f10e295e5152b9
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-18 18:54:02 -0400

    added questions and answers for py and js

--- pythonScripts.py
+++ pythonScripts.py
@@ -1181,3 +1181,58 @@ class Solution(object):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
+# Same Tree
+
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+# Example 1:
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+
+# Example 2:
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+
+# Example 3:
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in both trees is in the range [0, 100].
+# -104 <= Node.val <= 104
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        # Base case: If both nodes are None, the trees are the same
+        if not p and not q:
+            return True
+        
+        # If one node is None and the other is not, the trees are not the same
+        if not p or not q:
+            return False
+        
+        # If the values of the current nodes are different, the trees are not the same
+        if p.val != q.val:
+            return False
+        
+        # Recursively check the left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
