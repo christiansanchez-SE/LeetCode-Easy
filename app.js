a0f22ebcef10d5745159d1b4121dbebb48ae0d75
christiansanchez-SE <christiansanchez714@gmail.com>
2024-08-23 13:04:39 -0400

    added py and js questions and answers

--- app.js
+++ app.js
@@ -795,3 +795,36 @@ var maxDepth = function(root) {
 };
 
 //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val === undefined ? 0 : val);
+ *     this.left = (left === undefined ? null : left);
+ *     this.right = (right === undefined ? null : right);
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    if (!nums.length) return null;
+
+    function convertListToBST(left, right) {
+        if (left > right) return null;
+
+        let mid = Math.floor((left + right) / 2);
+        let node = new TreeNode(nums[mid]);
+
+        node.left = convertListToBST(left, mid - 1);
+        node.right = convertListToBST(mid + 1, right);
+
+        return node;
+    }
+
+    return convertListToBST(0, nums.length - 1);
+};
+
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
